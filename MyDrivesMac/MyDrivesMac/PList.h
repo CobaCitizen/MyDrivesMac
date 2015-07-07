a4c455c57469b878b042895423ad131d6f4de2e4
Maxim Bukshovan <jonentony@mail.ru>
2015-07-07 16:58:52 +0300

    PList

--- MyDrivesMac/MyDrivesMac/PList.h
+++ MyDrivesMac/MyDrivesMac/PList.h
@@ -1,13 +1,13 @@
 //
-//  Server.h
+//  PList.h
 //  MyDrivesMac
 //
-//  Created by Vasyl Bukshovan on 06/07/15.
+//  Created by Vasyl Bukshovan on 07/07/15.
 //  Copyright (c) 2015 cobasoft. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
-@interface Server : NSObject
+@interface PList : NSObject
 
 @end
---
+++ MyDrivesMac/MyDrivesMac/PList.m
@@ -0,0 +1,13 @@
+//
+//  PList.m
+//  MyDrivesMac
+//
+//  Created by Vasyl Bukshovan on 07/07/15.
+//  Copyright (c) 2015 cobasoft. All rights reserved.
+//
+
+#import "PList.h"
+
+@implementation PList
+
+@end
